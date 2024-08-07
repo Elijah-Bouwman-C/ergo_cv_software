df7fa56b004f1af6c8ef5589997ee75f3fa9c5df
egcvc <william.anderson3@collins.com>
2024-08-07 08:51:56 -0500

    Update desk_app_utils.py
    Delete unnecessary functions and add in more exports

--- CUSA/desk_app_utils.py
+++ CUSA/desk_app_utils.py
@@ -27,36 +27,6 @@ def blur_face(image_rgb,frame,face_detection):
     return frame
 
 
-#Counts frequency of maximum ejms score per category
-def ejms_counter(df):
-    count_dict = []
-    keyset = df.keys()
-    for key in keyset:
-        count_dict.append(df[df[key]==df[key].max()][key].count())
-    overall_df = pd.DataFrame.from_dict({1:count_dict},orient='index',columns = keyset)
-    return overall_df
-
-
-
-#calculate height to pixel ratio
-def calculate_pix_height(neck,ankle,person_h):
-    if neck and ankle:
-        pixel_height = abs(neck[1] - ankle[1])
-        pixels_per_foot = pixel_height/person_h
-        return pixels_per_foot 
-
-
-
-#uses the magnitude of 2 points to get distance
-def horizontal_dist(points,prev_points,pixels_per_foot):
-    points = np.array(points)
-    prev_points = np.array(prev_points)
-    pixel_distance = np.linalg.norm(np.array(points)-np.array(prev_points))
-    dist_carried = abs(pixel_distance/pixels_per_foot)
-
-    return dist_carried
-
-
 
 #arccos of 2 points with a 3rd as a reference to get angle
 def calculate_angle(point1, point2, point3):
@@ -70,72 +40,6 @@ def calculate_angle(point1, point2, point3):
     angle = np.arccos(np.clip(np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc)), -1.0, 1.0))
     return int(np.degrees(angle))
 
-
-
-
-#main function for assessing the ejms process
-def overall_assess_ejms(ejms_params):
-    neck,shoulder_angle,trunk_angle,wrist_angle,trunk_tort,leg_angle,ld,dist_carried,slp,start_time = ejms_params
-    
-    neck_score = 0 
-    if neck>5:
-        neck_score+=1
-        if neck > 20:
-            neck_score +=1
-    
-    trunk_score = 0
-    if trunk_angle>10 or trunk_tort >10:
-        trunk_score+=1
-        if trunk_angle>20 or trunk_tort>20:
-            trunk_score+=1
-    
-    shoulder_score = 0 
-    if shoulder_angle >30:
-        shoulder_score+=1
-        if shoulder_angle>90:
-            shoulder_score+=1
-    
-    leg_score = 0 
-    if leg_angle > 90:
-        leg_score+=1
-        if leg_angle >20:
-            leg_score+=1
-    
-    slp_score = 0
-    if slp > 1:
-        slp_score+=1
-        if slp>9:
-            slp_score+=1
-            if slp>11:
-                slp_score+=1
-                if slp>13:
-                    slp_score+=1
-
-    dist_score = 0
-    if dist_carried >= 1:
-        dist_score+=1
-        if dist_carried >=6:
-            dist_score+=1 
-            if dist_carried >=14:
-                dist_score+=1
-                if dist_carried >= 21:
-                    dist_score+=1
-    
-    ld_score = 0
-    if ld>0:
-        ld_score+=1
-        if ld>=15:
-            ld_score+=1
-            if ld>23:
-                ld_score+=1
-                if ld>36:
-                    ld_score+=1
-    
-    overall = neck_score+trunk_score+shoulder_score+leg_score+slp_score+dist_score+ld_score
-    overall_df = pd.DataFrame.from_dict({int(time.time()-start_time):[neck_score,trunk_score,shoulder_score,leg_score,slp_score,dist_score,ld_score,overall]},
-    orient='index',columns = ['neck_score','trunk_score','shoulder_score','leg_score','slp_score','dist_score','ld_score','overall'])
-    # overall_df.index.name = 'Time(S)'
-    return overall_df
 
 
 #This mostly just filters information input and output 
@@ -153,29 +57,16 @@ def fix_angle(angle,change=180):
     return abs(angle-change)
 
 
+
 #This just saves data neatly
 def post_process(datasets):
-    dfs,ejms_dfs,left_ejms_dfs,right_ejms_dfs,video_path = datasets
+    dfs,left_dfs,right_dfs,video_path = datasets
     dfs.index.name = 'Time(S)'
 
-    left_ejms_counts = individual_ejms_counts(left_ejms_dfs)
-    right_ejms_counts = individual_ejms_counts(right_ejms_dfs)
-
-    #this needs to be one excel file
-    overall_ejms_counts = ejms_counter(ejms_dfs)
-    dfs.to_csv(f'{video_path}_report.csv')
-
-    with pd.ExcelWriter(f'{video_path}_EJMS_breakdown.xlsx',mode='w') as writer:  
-        left_ejms_dfs.to_excel(writer,sheet_name='left_ejms')
-        right_ejms_dfs.to_excel(writer,sheet_name='right_ejms')
-        left_ejms_counts.to_excel(writer,sheet_name='left_ejms_counts')
-        right_ejms_counts.to_excel(writer,sheet_name='right_ejms_counts')
-
-
-#this will get the ejms counts of each category
-def individual_ejms_counts(df):
-    overall_df = df.apply(pd.Series.value_counts).fillna(0).astype(int)
-    return overall_df
+    with pd.ExcelWriter(f'{video_path}_desk_breakdown.xlsx',mode='w') as writer:  
+        dfs.to_excel(writer,sheet_name='overall')
+        right_dfs.to_excel(writer,sheet_name='right_side')
+        left_dfs.to_excel(writer,sheet_name='left_side')
 
 
 
@@ -193,6 +84,7 @@ def get_desk_params(params):
                 problematic_areas += f'{param}, '
     problematic_areas = problematic_areas.replace('_',' ')
     return problematic_areas[:-2]
+
 
 
 #This is where limbs are defined in relation to joints
@@ -249,28 +141,13 @@ def define_angles(mp_pose):
         mp_pose.PoseLandmark.LEFT_ANKLE.value
     ]
 
-    trunk_tor = [
-        mp_pose.PoseLandmark.RIGHT_SHOULDER.value,
-        mp_pose.PoseLandmark.RIGHT_HIP.value,
-        mp_pose.PoseLandmark.LEFT_HIP.value
-    ]
-    return trunk_tor,left_leg,left_lower_arm,left_upper_arm,trunk,neck,right_leg,right_lower_arm,right_upper_arm
-
-
-#this defines the distance deltas for vertical and horizontal lifts
-def define_distances(mp_pose,landmarks):
-    ld = [mp_pose.PoseLandmark.LEFT_WRIST.value,mp_pose.PoseLandmark.LEFT_ANKLE.value]
-    top_bottom = [mp_pose.PoseLandmark.LEFT_EAR.value,mp_pose.PoseLandmark.LEFT_ANKLE.value]
-    dist = [mp_pose.PoseLandmark.LEFT_ANKLE.value]
-    mid_pt = [mp_pose.PoseLandmark.LEFT_HIP.value]
-    new_dist =  np.array([landmarks[dist[0]].x,landmarks[dist[0]].y])
-    return ld,top_bottom,dist,mid_pt,new_dist
+    return left_leg,left_lower_arm,left_upper_arm,trunk,neck,right_leg,right_lower_arm,right_upper_arm
 
 
 
 #Calculates the joint angles
 def make_angles(landmarks,limb_sets):
-    trunk_tor,left_leg,left_lower_arm,left_upper_arm,trunk,neck,right_leg,right_lower_arm,right_upper_arm = limb_sets
+    left_leg,left_lower_arm,left_upper_arm,trunk,neck,right_leg,right_lower_arm,right_upper_arm = limb_sets
     neck_angle = fix_angle(calculate_angle(
                     (landmarks[neck[0]].x, landmarks[neck[0]].y),
                     (landmarks[neck[1]].x, landmarks[neck[1]].y),
@@ -282,12 +159,6 @@ def make_angles(landmarks,limb_sets):
         (landmarks[trunk[1]].x, landmarks[trunk[1]].y),
         (landmarks[trunk[2]].x, landmarks[trunk[2]].y)
     ))
-    
-    trunk_tort = fix_angle(calculate_angle(
-        (landmarks[trunk_tor[0]].x, landmarks[trunk_tor[0]].y),
-        (landmarks[trunk_tor[1]].x, landmarks[trunk_tor[1]].y),
-        (landmarks[trunk_tor[2]].x, landmarks[trunk_tor[2]].y)
-    ),90)
 
     right_upper_arm_angle = calculate_angle(
         (landmarks[right_upper_arm[0]].x, landmarks[right_upper_arm[0]].y),
@@ -324,4 +195,4 @@ def make_angles(landmarks,limb_sets):
         (landmarks[left_leg[1]].x, landmarks[left_leg[1]].y),
         (landmarks[left_leg[2]].x, landmarks[left_leg[2]].y)
     ))
-    return left_leg_angle,right_leg_angle,left_upper_arm_angle,right_upper_arm_angle,left_lower_arm_angle,right_lower_arm_angle,trunk_angle,trunk_tort,neck_angle
+    return left_leg_angle,right_leg_angle,left_upper_arm_angle,right_upper_arm_angle,left_lower_arm_angle,right_lower_arm_angle,trunk_angle,neck_angle
